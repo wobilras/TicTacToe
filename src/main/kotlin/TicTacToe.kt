3ab3b6a117560f53ca10dc24c430bdede76189bc
Wobilras <wobilras@yandex.ru>
2023-10-01 18:16:47 +0300

    added move and switch player

--- src/main/kotlin/TicTacToe.kt
+++ src/main/kotlin/TicTacToe.kt
@@ -17,4 +17,19 @@ class TicTacToe {
             println()
         }
     }
+    fun makeMove(symbol: String, row: Int, col: Int): Boolean {
+        if (row < 0 || row >= 3 || col < 0 || col >= 3 || board[row][col] != "-") {
+            return false
+        }
+        board[row][col] = symbol
+        return true
+    }
+
+    fun switchPlayer(symbol: String): String {
+        return if (symbol == "X") {
+            "O"
+        } else {
+            "X"
+        }
+    }
 }
